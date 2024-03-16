cde61989435c7d64f783ddbd48d0dd4fb7363e69
Andrés Martínez <autummata@gmail.com>
2024-03-15 17:12:16 -0700

    Add: Connection to Posgresql DB

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import express, { type Express } from 'express'
 import apiRouter from './src/routes/index'
 import morgan from 'morgan'
+import connectionDB from './src/connection/connection'
 
 const app: Express = express()
-
 const port = 3007
 
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(apiRouter)
+
+connectionDB()
 
 app.listen(port, () => { console.log(`localhost:${port}`) })
